e1cf768787ffc5d42f961c27f66580423b189cab
Jamie Maclennan <22000375@uhi.ac.uk>
2025-04-06 16:50:28 +0100

    Implemented add & preliminary alarm trigger

---
+++ ClockV2/Alarm/AlarmHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriorityQueue;
+
+namespace ClockV2.Alarm
+{
+    internal class AlarmHandler
+    {
+        public AlarmHandler()
+        {
+            
+        }
+    }
+}
--- ClockV2/View/DialougeAdd.Designer.cs
+++ ClockV2/View/DialougeAdd.Designer.cs
@@ -33,30 +33,31 @@
             this.lblAddText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // dateTimePicker1
+            // DTPicker
             // 
             this.DTPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.DTPicker.Location = new System.Drawing.Point(12, 34);
-            this.DTPicker.Name = "dateTimePicker1";
+            this.DTPicker.MinDate = new System.DateTime(2025, 4, 6, 0, 0, 0, 0);
+            this.DTPicker.Name = "DTPicker";
             this.DTPicker.Size = new System.Drawing.Size(200, 20);
             this.DTPicker.TabIndex = 0;
             // 
-            // button1
+            // btnFormAdd
             // 
             this.btnFormAdd.Location = new System.Drawing.Point(218, 31);
-            this.btnFormAdd.Name = "button1";
+            this.btnFormAdd.Name = "btnFormAdd";
             this.btnFormAdd.Size = new System.Drawing.Size(75, 23);
             this.btnFormAdd.TabIndex = 1;
             this.btnFormAdd.Text = "Add";
             this.btnFormAdd.UseVisualStyleBackColor = true;
             this.btnFormAdd.Click += new System.EventHandler(this.btnFormAddClick);
             // 
-            // label1
+            // lblAddText
             // 
             this.lblAddText.AutoSize = true;
             this.lblAddText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.lblAddText.Location = new System.Drawing.Point(12, 9);
-            this.lblAddText.Name = "label1";
+            this.lblAddText.Name = "lblAddText";
             this.lblAddText.Size = new System.Drawing.Size(101, 17);
             this.lblAddText.TabIndex = 2;
             this.lblAddText.Text = "Add new alarm";
@@ -71,7 +72,6 @@
             this.Controls.Add(this.DTPicker);
             this.Name = "DialougeAdd";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.DialougeAdd_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
